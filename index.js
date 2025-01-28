ae4c1c5a1d348bcc8d3ca39d89c31998bed2e858
Madcz12 <diamondmiguel12@gmail.com>
2025-01-28 17:17:02 -0400

    Agregando backend MERN

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+require('dotenv').config();
+const cors = require('cors');
+const {dbConnection} = require('./database/config');
+
+
+// crear el servidor de express: 
+
+const app = express(); 
+
+// Base de Datos 
+dbConnection();
+
+app.use(cors())
+
+// Directorio público
+app.use(express.static('public'));
+
+// Lectura y parseo del body 
+
+app.use(express.json());
+
+// rutas 
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/events', require('./routes/events'));
+// TODO CRUD: eventos 
+
+
+// escuchar peticiones 
+
+app.listen(process.env.PORT, () => {
+    console.log(`Servidor corriendo en puerto ${process.env.PORT}`);
+})
