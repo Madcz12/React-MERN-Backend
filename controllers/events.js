999b74d002388cd42663be6d23f65abb65158489
Madcz12 <diamondmiguel12@gmail.com>
2025-01-30 18:44:57 -0400

    React App fired up to ready to serve

--- controllers/events.js
+++ controllers/events.js
@@ -4,30 +4,22 @@ const Evento = require('../models/Evento');
 const { generarJWT } = require('../helpers/jwt');
 
 const getEventos = async(req, res = response) => {
-
     const eventos = await Evento.find().populate('user', 'name');
-
     res.json({
         ok: true,
-        msg: eventos
+        eventos
     });
 }
 
 const crearEvento = async(req, res = response) => {
-
     const evento = new Evento(req.body);
-
     try {
-
         evento.user = req.uid; 
-
         const eventoGuardado = await evento.save();
-
         res.json({
             ok: true,
             evento: eventoGuardado
-        })
-        
+        })    
     } catch (error) {
         console.log(error);
         res.status(500).json({
@@ -35,44 +27,35 @@ const crearEvento = async(req, res = response) => {
             msg: 'Hable con el admistrador'
         });
     }
-
 }
 
 const actualizarEvento = async(req, res = response) => {
-
     const eventoId = req.params.id;
     const uid = req.uid;
-
     try {
-
         const evento = await Evento.findById(eventoId);
-
         if(!evento){
             return res.status(404).json({
                 ok: false, 
                 msg: 'Evento no existe con ese id'
             });
         }
-
         if(evento.user.toString() !== uid) {
             return res.status(401).json({
                 ok: false,
                 msg: 'No tiene privilegio de editar este evento'
             });
         }
-
         const nuevoEvento = {
             ...req.body,
             user: uid
         }
-
         const eventoActualizado = await Evento.findByIdAndUpdate(eventoId, nuevoEvento, {new: true});
-
         res.json({
             ok: true,
             evento: eventoActualizado
         });
-        
+     
     } catch (error) {
         console.log(error);
         res.status(500).json({
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
+const path = require('path');
 const {dbConnection} = require('./database/config');
 
 
@@ -25,7 +26,9 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/events', require('./routes/events'));
 // TODO CRUD: eventos 
 
-
+app.use('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+});
 // escuchar peticiones 
 
 app.listen(process.env.PORT, () => {
